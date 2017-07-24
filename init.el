f09fff8aae413070f5ef567663b04a11e0974a96
Alain M. Lafon <alain.lafon@dispatched.ch>
2017-07-24 08:41:52 +0200

    Add custom-set-variables for mml-secure-key

--- init.el
+++ init.el
@@ -10,14 +10,25 @@
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
+
 (org-babel-load-file "~/.emacs.d/configuration.org")
 
 ;; custom-set-variables was added by Custom.
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+
+ '(mml-secure-key-preferences
+   (quote
+    ((OpenPGP
+      (sign)
+      (encrypt
+       ("tpo@sourcepole.ch" "549A598550F2C6DB416F287C9EF854EDFC992520")))
+     (CMS
+      (sign)
+      (encrypt))))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -25,5 +36,18 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; Select the next WP page_id from a list of IDs
+;; Premise in the buffer:
+;; Mark a: /* 14758 */
+;;
+;; /* 14762 14754 15609 15615 15623 15639 15627 15631 15635 */
+(fset 'select-next-page-id
+   "`bdw`cdw`bhpgg")
+
+;; Given the above setting, create new selectors for the new page-id
+(fset 'create-selectors-for-page-id
+   [?\C-s ?1 ?4 ?9 ?8 ?9 return ?y ?y ?p ?m ?d ?` ?b ?v ?e ?y ?` ?d ?V ?: ?s ?/ ?1 ?4 ?9 ?8 ?9 ?/ ?\C-y ?/ ?g return])
+
 
 ;;; init.el ends here
