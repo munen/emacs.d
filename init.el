1b339607e69d1e9a3b3f342088a0a0b670bc8cf0
Alain M. Lafon <alain.lafon@dispatched.ch>
2017-07-24 08:29:30 +0200

    Move `(package-initialize)` to init.el

--- init.el
+++ init.el
@@ -4,6 +4,12 @@
 ;;; Code:
 
 ;; This loads the actual configuration in literate org-mode elisp
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
 (org-babel-load-file "~/.emacs.d/configuration.org")
 
 ;; custom-set-variables was added by Custom.
