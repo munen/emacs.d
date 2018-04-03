aca1b70f6ef0688ed6f218e3cfc7caa665569b78
Alain M. Lafon <alain@200ok.ch>
2018-04-03 10:39:36 +0200

    Track custom settings separately

--- init.el
+++ init.el
@@ -1,12 +1,8 @@
 ;;; package --- Alain M. Lafon <alain@200ok.ch> Emacs config
 ;;; Commentary:
-;;   - Most of the configuration is within `configuration.org`
+;;   - All of the configuration is within `configuration.org`
 ;;; Code:
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
 (package-initialize)
 
 ;; This loads the actual configuration in literate org-mode elisp
@@ -15,48 +11,5 @@
   (org-babel-load-file "~/.emacs.d/configuration.org"))
 
 (load-config)
-
-;; custom-set-variables was added by Custom.
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(cider-boot-parameters "cider repl -s wait")
- '(mml-secure-key-preferences
-   (quote
-    ((OpenPGP
-      (sign)
-      (encrypt
-       ("tpo@sourcepole.ch" "549A598550F2C6DB416F287C9EF854EDFC992520")))
-     (CMS
-      (sign)
-      (encrypt)))))
- '(org-agenda-files
-   (quote
-    ("~/switchdrive/org/things.org" "~/switchdrive/org/inbox.org" "~/Dropbox/ZHAW/web3-unterlagen/README.org" "~/Dropbox/ZHAW/weng-unterlagen/README.org" "~/switchdrive/org/reference.org")))
- '(package-selected-packages
-   (quote
-    (browse-kill-ring clipmon rainbow-mode beacon js2-refactor graphviz-dot-mode js-comint intero haskell-mode comment-tags handlebars-mode json-mode mustache-mode seeing-is-believing evil-mc elfeed-goodies elfeed-org zenburn-theme writegood-mode writeroom-mode which-key darktooth-theme magit restclient impatient-mode evil-numbers evil-surround evil-leader evil smex ledger-mode robe enh-ruby-mode markdown-mode projectile coffee-mode tern-auto-complete tern pdf-tools yaml-mode sass-mode fixme-mode flycheck-flow ac-js2 js2-mode ac-cider exec-path-from-shell cider clj-refactor parinfer clojure-mode web-mode auto-complete flycheck ag))))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-;; Select the next WP page_id from a list of IDs
-;; Premise in the buffer:
-;; Mark a: /* 14758 */
-;;
-;; /* 14762 14754 15609 15615 15623 15639 15627 15631 15635 */
-(fset 'select-next-page-id
-   "`bdw`cdw`bhpgg")
-
-;; Given the above setting, create new selectors for the new page-id
-(fset 'create-selectors-for-page-id
-   [?\C-s ?1 ?4 ?9 ?8 ?9 return ?y ?y ?p ?m ?d ?` ?b ?v ?e ?y ?` ?d ?V ?: ?s ?/ ?1 ?4 ?9 ?8 ?9 ?/ ?\C-y ?/ ?g return])
-
 
 ;;; init.el ends here
