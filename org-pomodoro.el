217f755e509f7c5fbc6af1a749808b73f4156ab0
Alain M. Lafon <alain@200ok.ch>
2020-04-17 10:34:08 +0200

    feat: Ability to call `pomodoro-start` from any buffer

--- org-pomodoro.el
+++ org-pomodoro.el
@@ -13,10 +13,8 @@
 
 (defun set-start-timer ()
   "When the timer is over, let the user take a break!"
-
   (shell-command "say 'Time to take a break'")
   (message-box "Time to take a break"))
-
 
 (defun pomodoro-break ()
   "."
@@ -33,9 +31,18 @@
 (defun pomodoro-start ()
   "."
   (interactive)
-  (remove-hook 'org-timer-done-hook 'set-break-timer)
-  (add-hook 'org-timer-done-hook 'set-start-timer)
-  (org-timer-set-timer 25))
+  (let* ((starting-buffer-name (buffer-name (current-buffer)))
+         (switchp (not (string-equal starting-buffer-name ok-pomodoro-buffer))))
+    ;; If the current buffer is not the primary org pomodoro buffer,
+    ;; switch to the primary org buffer, so that the timer is attached
+    ;; there.
+    (when switchp
+        (switch-to-buffer ok-pomodoro-buffer))
+    (remove-hook 'org-timer-done-hook 'set-break-timer)
+    (add-hook 'org-timer-done-hook 'set-start-timer)
+    (org-timer-set-timer 25)
+    (when switchp
+        (switch-to-buffer (other-buffer)))))
 
 (defvar pomodoro-auto-clock-in t
   "When set to non-nil, a pomodoro will automatically be started when clocking in on any task in 'org-mode'.")
