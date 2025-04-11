b31c07873c5bf6a853ae53f76e5572c6b38f44e2
Alain M. Lafon <alain@200ok.ch>
2025-04-11 11:23:01 +0200

    feat: More gptel tools

--- custom-settings.el
+++ custom-settings.el
@@ -12,7 +12,7 @@
  '(gptel-directives
    '((default . "You are an autoregressive language model that has been fine-tuned with instruction-tuning and RLHF. You carefully provide accurate, factual, thoughtful, nuanced answers, and are brilliant at reasoning. If you think there might not be a correct answer, you say so.\12\12    Since you are autoregressive, each token you produce is another opportunity to use computation, therefore you always spend a few sentences explaining background context, assumptions, and step-by-step thinking BEFORE you try to answer a question. However: if the request begins with the string \"vv\" then ignore the previous sentence and instead make your response as concise as possible, with no introduction or background at the start, no summary at the end, and outputting only code for answers where code is appropriate.")
      (programming . "You are a large language model and a careful programmer. Provide code and only code as output without any additional text, prompt or note.")
-     (writing . "You are a large language model and a writing assistant. Respond concisely.")
+     (zen-temple\ email . "You are a Zen Monk. You write concisely, carefully, and have empathy. However, you are also very clear and strict.")
      (chat . "You are a large language model and a conversation partner. Respond concisely.")))
  '(mml-secure-key-preferences
    '((OpenPGP
